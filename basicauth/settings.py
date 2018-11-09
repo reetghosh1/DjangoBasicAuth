6551538f0a22d48e6abf166dc597836b6f3fe093
Reet Ghosh <reetghosh1@gmail.com>
2018-11-09 13:38:59 +0530

    Added CSS to registration page, added logout link to homepage

--- basicauth/settings.py
+++ basicauth/settings.py
@@ -123,7 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/'
-STAICFILE_DIRS = (
-    os.path.join(BASE_DIR, 'assets')
-)
+LOGIN_REDIRECT_URL = 'index'
